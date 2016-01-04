6e02d3f5c3e53a21e5d9c4b133bf613647122568
Lectem <lectem@gmail.com>
2016-01-04 15:03:01 -0500

    Added TLS flags for latest dkA

--- DevkitArm3DS.cmake
+++ DevkitArm3DS.cmake
@@ -52,7 +52,7 @@ SET(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Shared libs not available" )
 
 add_definitions(-DARM11 -D_3DS)
 
-set(ARCH "-march=armv6k -mtune=mpcore -mfloat-abi=hard ")
+set(ARCH "-march=armv6k -mtune=mpcore -mfloat-abi=hard  -mtp=soft ")
 set(CMAKE_C_FLAGS " -mword-relocations ${ARCH}" CACHE STRING "C flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "C++ flags")
 set(DKA_SUGGESTED_C_FLAGS "-fomit-frame-pointer -ffast-math")
